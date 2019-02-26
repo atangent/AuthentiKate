3def89a7365089a9d58cb3ae17bfdb96938f70b1
Amy Tang <amytxng@gmail.com>
2019-02-25 19:44:35 -0500

    upload training classifier

---
+++ train.py
@@ -0,0 +1,23 @@
+from io_stuff import read_data
+
+def average(lst):
+    'Generic average'
+    return sum(lst)/len(lst)
+
+def train():
+    'Train a classifier and returns it'
+    data = read_data('training.txt')
+    zero = []
+    one = []
+    # creating subsets
+    for element in data:
+        if int(element[-1]) == 1: zero.append(element)
+        else: one.append(element)
+
+    clf = []
+    for i in range(4):
+        z = average([float(n[i]) for n in zero])
+        o = average([float(n[i]) for n in one])
+        clf.append(average([z, o]))
+
+    return clf
